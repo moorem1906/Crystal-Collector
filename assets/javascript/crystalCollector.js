008885ebd556760230f1498270cb00c66ee16566
moorem1906 <moorem1906@gmail.com>
2019-06-05 10:25:38 -0400

    Added computer and user random number

---
+++ assets/javascript/crystalCollector.js
@@ -0,0 +1,63 @@
+// create an array with random numbers
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+
+$("#randomNumberMessage").append(randomNumber);
+
+var randomNum = [];
+
+$("#randomNum")
+function randomNumberMessage() { 
+    document.getElementById("randomNumberMessage").innerHTML = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumberMessage);
+}
+
+// assign random number to crystal image 
+
+var crystalNum = Math.floor(Math.random() * 50) + 1;
+console.log(crystalNum);
+
+$("#image1").append(crystalNum);
+
+var crystalRandom = [];
+
+var onclick = $("#image1").click(function image1() { 
+    document.getElementById("image1").innerHTML = Math.floor(Math.random() * 50) + 1;
+    console.log(image1);
+    
+});
+
+
+
+
+
+//$("#crystalRandom")
+//function image1(){
+   //document.getElementById("#image1").innerHTML = Math.floor(Math.random() * 50) + 1;
+  // console.log(image1);
+//}
+
+
+// click on crystal to select random number 
+// when user click on crystal the number is added to random number
+// when user click on another crystal that number is also added
+// if user match random number the user wins
+    // user win column is increased by one increment
+    // user is alerted with you've won 
+    // add animation (happy dance!)
+    // generated another random number guess
+    // generated random number for crystal
+// if user exceeds random number the user lose 
+    //user lose colunm is increased by one increment
+    // user is alerted with you've lose
+    // add animation (sad face!)
+    // generated another random number guess
+    // generated random number for crystal
+
+
+
+   
+ 
+//choose number randomly with the math.floor 
+
+$
